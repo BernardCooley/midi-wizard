497e72842e6fc354a142097ff34d30c30a0d7236
Bernard Cooley <>
2020-03-24 23:15:06 +0000

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,6 @@ const Header = () => {
 
     const dispatch = useDispatch();
     const isAdmin = useSelector(state => state.isAdmin);
-    const isLoggedIn = useSelector(state => state.isLoggedIn);
     const isAdminConsoleOpen = useSelector(state => state.isAdminConsoleOpen);
 
     const logout = () => {
@@ -22,19 +21,44 @@ const Header = () => {
         headerContainer: {
             width: '100%',
             backgroundColor: 'green',
-            height: '50px'
+            height: '50px',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+        },
+        button: {
+            height: '100%',
+            margin: '0 10px',
+            cursor: 'pointer',
+            width: '100%'
+        },
+        appTitle: {
+            color: 'white'
+        },
+        buttonContainer: {
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '30px',
+            width: '115px'
+        },
+        titleContainer: {
+
         }
     }
 
     return (
         <div style={styles.headerContainer}>
-
-            {isAdmin && !isAdminConsoleOpen ? <button onClick={openAdminConsole}>Admin console</button> : null}
-
-            {isLoggedIn ? 
-                <button onClick={logout}>Logout</button> : null
-            }
-            Header
+            <div style={styles.buttonContainer}>
+                {isAdmin && !isAdminConsoleOpen ? <button  style={styles.button} onClick={openAdminConsole}>Admin console</button> : null}
+            </div>
+            <div style={styles.titleContainer}>
+                <h1 style={styles.appTitle}>Studio Designer</h1>
+            </div>
+            <div  style={styles.buttonContainer}>
+                <button style={styles.button} onClick={logout}>Logout</button>
+            </div>
         </div>
     )
 }
