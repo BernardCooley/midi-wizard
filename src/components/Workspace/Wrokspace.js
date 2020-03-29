1c4e15281e12f390f985437276ae84e95d39c711
Bernard Cooley <>
2020-03-29 16:44:25 +0100

    workspace not updating fixed

--- src/components/Workspace/Wrokspace.js
+++ src/components/Workspace/Wrokspace.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import firebase from 'firebase';
 import LayoutsTray from '../LayoutsTray/LayoutsTray';
+import { currentLayout } from '../../actions';
 
 const Workspace = () => {
 
     const db = firebase.firestore();
     const userLayoutDataRef = db.collection('UserLayouts');
+
+    const dispatch = useDispatch();
     const layout = useSelector(state => state.currentLayout);
+    const userLayouts = useSelector(state => state.layouts);
+    const currentLayoutId = useSelector(state => state.selectedLayoutId);
+
+    useEffect(() => {
+        if(userLayouts.length > 0) {
+            console.log(currentLayoutId);
+            if(currentLayoutId) {
+                dispatch(currentLayout(userLayouts.filter(layout => layout.layoutId === currentLayoutId)[0]));
+            }
+        }
+    }, [userLayouts]);
+
+    useEffect(() => {
+        if(userLayouts.length > 0) {
+            dispatch(currentLayout(userLayouts.filter(layout => layout.layoutId === currentLayoutId)[0]));
+        }
+    }, [currentLayoutId]);
 
     const removeFromLayout = async e => {
         const confirmDelete = window.confirm("Delete from layout");
