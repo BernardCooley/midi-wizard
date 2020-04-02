86e9d95e119071763a92ab0c44e5f14a1fa31030
Bernard Cooley <>
2020-04-02 23:05:39 +0100

    username added to header header styled

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import firebase from '../../firebase';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleAdminConsole } from '../../actions';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+    .headerContainer {
+        width: 100%;
+        background-color: rebeccapurple;
+        height: 50px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: absolute;
+        border-bottom: 1px solid #666666;
+
+        .buttonContainer {
+            height: 100%;
+            align-items: center;
+            justify-content: center;
+            flex-grow: 1;
+            flex-basis: 0;
+            height: 100%;
+            display: flex;
+            justify-content: flex-start;
+        }
+
+        .button {
+            height: auto;
+            margin: 0 10px;
+            cursor: pointer;
+            width: auto;
+            outline: none;
+            padding: 8px;
+            font-size: 16px;
+            background-color: #383838;
+            color: white;
+        }
+
+        .buttonHighlighed {
+            outline: 3px solid #2ef12b;
+        }
+
+        .titleContainer {
+            flex-grow: 1;
+            flex-basis: 0;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            .appTitle {
+                color: white;
+                font-size: 30px;
+                text-align: center;
+                font-weight: bold;
+            }
+        }
+
+        .accountActionsContainer {
+            flex-grow: 1;
+            flex-basis: 0;
+            height: 100%;
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+
+            .userName {
+                color: #cecece;
+            }
+        }
+    }
+`
 
 const Header = () => {
 
     const dispatch = useDispatch();
     const isAdmin = useSelector(state => state.isAdmin);
     const isAdminConsoleOpen = useSelector(state => state.isAdminConsoleOpen);
+    const currentUsername = useSelector(state => state.currentUsername);
+    const [firstName, setFirstName] = useState('');
+
+    useEffect(() => {
+        setFirstName(getFirstName());
+    }, [currentUsername]);
 
     const logout = () => {
         firebase.auth().signOut();
+    }
+
+    const getFirstName = () => {
+        return currentUsername.replace(/ .*/,'');
     }
 
     const openCloseAdminConsole = () => {
@@ -21,57 +101,23 @@ const Header = () => {
         }
     }
 
-    const styles = {
-        headerContainer: {
-            width: '100%',
-            backgroundColor: 'green',
-            height: '50px',
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            position: 'absolute'
-        },
-        button: {
-            height: '100%',
-            margin: '0 10px',
-            cursor: 'pointer',
-            width: '100%',
-            outline: 'none'
-        },
-        adminOpen: {
-            backgroundColor: '#383838',
-            color: 'white'
-        },
-        appTitle: {
-            color: 'white'
-        },
-        buttonContainer: {
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '30px',
-            width: '115px'
-        },
-        titleContainer: {
-
-        }
-    }
-
     return (
-        <div style={styles.headerContainer}>
-            <div style={styles.buttonContainer}>
-                {isAdmin ? 
-                    <button  style={{...styles.button, ...isAdminConsoleOpen ? styles.adminOpen : ''}} onClick={openCloseAdminConsole}>Admin console</button>: null
-                }
+        <Styles>
+            <div className='headerContainer'>
+                <div className='buttonContainer'>
+                    {isAdmin ? 
+                        <button className={`button ${isAdminConsoleOpen ? 'buttonHighlighed' : ''}`} onClick={openCloseAdminConsole}>Admin console</button>: null
+                    }
+                </div>
+                <div className='titleContainer'>
+                    <div className='appTitle'>Studio Designer</div>
+                </div>
+                <div className='accountActionsContainer'>
+                    <div className='userName'>Welcome {firstName}</div>
+                    <button className='button' onClick={logout}>Logout</button>
+                </div>
             </div>
-            <div style={styles.titleContainer}>
-                <h1 style={styles.appTitle}>Studio Designer</h1>
-            </div>
-            <div  style={styles.buttonContainer}>
-                <button style={styles.button} onClick={logout}>Logout</button>
-            </div>
-        </div>
+        </Styles>
     )
 }
 
