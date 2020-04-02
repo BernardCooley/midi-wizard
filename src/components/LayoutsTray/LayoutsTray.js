c20eb7e0b49c9fab63524ab76d6c265afd925f3c
Bernard Cooley <>
2020-04-03 00:15:28 +0100

    styled component layout tray

--- src/components/LayoutsTray/LayoutsTray.js
+++ src/components/LayoutsTray/LayoutsTray.js
@@ -5,6 +5,128 @@ import { faTrashAlt, faEdit, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { useSelector, useDispatch } from 'react-redux';
 import { selectedLayoutId, isLayoutsTrayOpen, currentLayout, deletedLayoutId } from '../../actions';
 import firebase from 'firebase';
+import styled from 'styled-components';
+
+
+const Styles = styled.div`
+    .layoutsTrayOuterContainer {
+        position: fixed;
+        top: 50%;
+        right: 0;
+        transform: translate(0, -50%);
+        display: flex;
+        align-items: center;
+        transition:0.6s;
+        -webkit-transition:0.6s;
+        -moz-transition:0.6s;
+
+        .layoutsTabOpenClose {
+            transform: rotate(-90deg);
+            background-color: white;
+            height: 30px;
+            border: none;
+            width: 120px;
+            font-size: 20px;
+            outline: none;
+            border-radius: 5px 5px 0px 0px;
+            cursor: pointer;
+            position: relative;
+            right: -45px;
+        }
+
+        .layoutsTrayContainer {
+            height: 305px;
+            width: auto;
+            background-color: white;
+            padding: 10px;
+            overflow: hidden;
+            position: relative;
+
+            .layoutsListContainer {
+                display: flex;
+                flex-direction: column;
+                height: 90%;
+                justify-content: flex-start;
+                align-items: flex-start;
+                overflow-x: hidden;
+                overflow-y: scroll;
+                position: absolute;
+                top: 40px;
+
+                .layoutContainer {
+                    width: 100%;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    margin: 5px 0;
+                    border-bottom: 1px solid gray;
+
+                    .layoutNameContainer {
+                        cursor: pointer;
+
+                        .layoutName {
+                            pointer-events: none;
+                            border: none;
+                            font-size: 14px;
+                        }
+
+                        .editEnabled {
+                            pointer-events: all;
+                        }
+                    }
+
+                    .layoutActions {
+                        display: flex;
+                        flex-direction: row;
+
+                        .layoutDeviceActionContainer {
+                            margin: 3px;
+
+                            .editIcon {
+                                color: blue;
+                            }
+
+                            .deleteIcon {
+                                color: red;
+                            }
+                        }
+                    }
+
+                    .svg {
+                        pointer-events: none;
+                    }
+                }
+
+                .layoutSelected {
+                    border: 1px solid lightgreen;
+                }
+            }
+
+            .newLayoutButtonsContainer {
+                display: flex;
+                justify-content: space-between;
+                position: relative;
+
+                .createNewLayoutButton {
+                    width: 90px;
+                }
+
+                .newLayoutButtons {
+                    height: 25px;
+                    font-size: 13px;
+                }
+
+                .copyLayoutButton {
+                    width: 100px;
+                }
+            }
+        }
+    }
+
+    .hidden {
+        right: -210px;
+    }
+`
 
 const LayoutsTray = () => {
 
@@ -123,142 +245,39 @@ const LayoutsTray = () => {
         addNewLayoutToUserLayouts(addNewLayoutToLayouts(true, selectedLayout));
     }
 
-    const styles = {
-        layoutsTrayOuterContainer: {
-            position: 'fixed',
-            top: '50%',
-            right: '0',
-            transform: 'translate(0, -50%)',
-            display: 'flex',
-            alignItems: 'center',
-            transition:'0.6s',
-            EebkitTransition:'0.6s',
-            MozTransition:'0.6s',
-        },
-        layoutsTrayContainer: {
-            height: '305px',
-            width: 'auto',
-            backgroundColor: 'white',
-            padding: '10px',
-            overflow: 'hidden',
-            position: 'relative'
-        },
-        layoutsTabOpenClose: {
-            transform: 'rotate(-90deg)',
-            backgroundColor: 'white',
-            height: '30px',
-            border: 'none',
-            width: '120px',
-            fontSize: '20px',
-            outline: 'none',
-            borderRadius: '5px 5px 0px 0px',
-            cursor: 'pointer',
-            position: 'relative',
-            right: '-45px'
-        },
-        layoutsListContainer: {
-            display: 'flex',
-            flexDirection: 'column',
-            height: '90%',
-            justifyContent:' flex-start',
-            alignItems:' flex-start',
-            overflowX: 'hidden',
-            overflowY: 'scroll',
-            position: 'absolute',
-            top: '40px'
-        },
-        layoutContainer: {
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            margin: '5px 0',
-            borderBottom: '1px solid gray'
-        },
-        layoutNameContainer: {
-            cursor: 'pointer'
-        },
-        layoutName: {
-            pointerEvents: 'none',
-            border: 'none',
-            fontSize: '14px'
-        },
-        layoutActions: {
-            display: 'flex',
-            flexDirection: 'row'
-        },
-        svg: {
-            pointerEvents: 'none'
-        },
-        layoutDeviceActionContainer: {
-            margin: '3px'
-        },
-        deleteIcon: {
-            color: 'red'
-        },
-        editIcon: {
-            color: 'blue'
-        },
-        confirmEditIcon: {
-            color: 'green'
-        },
-        hidden: {
-            right: '-210px'
-        },
-        layoutSelected: {
-            border: '1px solid lightgreen'
-        },
-        editEnabled: {
-            pointerEvents: 'all'
-        },
-        newLayoutButtonsContainer: {
-            display: 'flex',
-            justifyContent: 'space-between',
-            position: 'relative'
-        },
-        newLayoutButtons: {
-            height: '25px',
-            fontSize: '13px'
-        },
-        createNewLayoutButton: {
-            width: '90px'
-        },
-        copyLayoutButton: {
-            width: '100px'
-        }
-    }
-
     return (
-        <div style={{...styles.layoutsTrayOuterContainer, ...!layoutsTrayOpen ? styles.hidden : ''}} className='layoutsTrayOuterContainer'>
-            <button onClick={toggleLayoutsTray} style={styles.layoutsTabOpenClose} className='layoutsTabOpenClose' onClick={toggleLayoutsTray}>Layouts</button>
-            <div className='layoutsTrayContainer' style={styles.layoutsTrayContainer}>
-                <div style={styles.layoutsListContainer} className='layoutsListContainer'>
-                    {userLayouts.map((layout, index) => (
-                        layout ? 
-                        <div layoutid={layout.layoutId} key={index} style={{...styles.layoutContainer, ...currentLayoutId === layout.layoutId ? styles.layoutSelected : ''}} className='layoutContainer'>
-                            <div onClick={switchLayouts} style={styles.layoutNameContainer} className='layoutNameContainer'>
-                                <input onChange={updateLayoutNameField} type='text' value={editEnabled && layoutIdBeingEdited === layout.layoutId ? layoutName : layout.layoutName} style={{...styles.layoutName, ...editEnabled ? styles.editEnabled : ''}} className='layoutName'/>
-                            </div>
-                            <div style={styles.layoutActions} className='layoutActions'>
-                                <div style={styles.layoutDeviceActionContainer} onClick={renameLayout} className='layoutDeviceActionContainer'>
-                                    {editEnabled && layoutIdBeingEdited === layout.layoutId ?
-                                        <FontAwesomeIcon style={{...styles.svg, ...styles.editIcon}} icon="check" />:
-                                        <FontAwesomeIcon style={{...styles.svg, ...styles.editIcon}} icon="edit" />
-                                    }
+        <Styles>
+            <div className={`layoutsTrayOuterContainer ${!layoutsTrayOpen ? 'hidden' : ''}`}>
+                <button onClick={toggleLayoutsTray} className='layoutsTabOpenClose' onClick={toggleLayoutsTray}>Layouts</button>
+                <div className='layoutsTrayContainer'>
+                    <div className='layoutsListContainer'>
+                        {userLayouts.map((layout, index) => (
+                            layout ? 
+                            <div layoutid={layout.layoutId} key={index} className={`layoutContainer ${currentLayoutId === layout.layoutId ? 'layoutSelected' : ''}`}>
+                                <div onClick={switchLayouts} className='layoutNameContainer'>
+                                    <input onChange={updateLayoutNameField} type='text' value={editEnabled && layoutIdBeingEdited === layout.layoutId ? layoutName : layout.layoutName} className={`layoutName ${editEnabled ? 'editEnabled' : ''}`}/>
                                 </div>
-                                <div style={styles.layoutDeviceActionContainer} onClick={deleteFromLayout} className='layoutDeviceActionContainer'>
-                                    <FontAwesomeIcon style={{...styles.svg, ...styles.deleteIcon}} icon="trash-alt" />
+                                <div className='layoutActions'>
+                                    <div onClick={renameLayout} className='layoutDeviceActionContainer'>
+                                        {editEnabled && layoutIdBeingEdited === layout.layoutId ?
+                                            <FontAwesomeIcon className='svg editIcon' icon="check" />:
+                                            <FontAwesomeIcon className='svg editIcon' icon="edit" />
+                                        }
+                                    </div>
+                                    <div onClick={deleteFromLayout} className='layoutDeviceActionContainer'>
+                                        <FontAwesomeIcon className='svg deleteIcon' icon="trash-alt" />
+                                    </div>
                                 </div>
-                            </div>
-                        </div>: null
-                    ))}
-                </div>
-                <div style={styles.newLayoutButtonsContainer} className='newLayoutButtonsContainer'>
-                    <button onClick={addNewLayout} style={{...styles.createNewLayoutButton, ...styles.newLayoutButtons}} className='createNewLayoutButton'>New layout</button>
-                    <button onClick={copyLayout} style={{...styles.copyLayoutButton, ...styles.newLayoutButtons}} className='createNewLayoutButton'>Copy to new</button>
+                            </div>: null
+                        ))}
+                    </div>
+                    <div className='newLayoutButtonsContainer'>
+                        <button onClick={addNewLayout} className='createNewLayoutButton newLayoutButtons'>New layout</button>
+                        <button onClick={copyLayout} className='copyLayoutButton newLayoutButtons'>Copy to new</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Styles>
     )
 }
 
