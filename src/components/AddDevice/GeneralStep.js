f9bfa83da0a0505d30312dcbc3cd3d728040dde6
BernardCooley <bernardcooley@gmail.com>
2020-05-04 16:01:04 +0100

    field type checkboxes

--- src/components/AddDevice/GeneralStep.js
+++ src/components/AddDevice/GeneralStep.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { addDeviceFormValues, currentStep } from '../../actions';
 import { useSelector, useDispatch } from 'react-redux';
 import StepNavigationButton from './StepNavigationButton';
-import { AddDeviceFormStyles } from '../../styles/components';
+import { AddDeviceFormStyles, CustomCheckBoxStyles } from '../../styles/components';
+import Colors from '../../styles/colors';
 
 const Styles = styled.div`
     width: 90%;
@@ -12,15 +13,86 @@ const Styles = styled.div`
     margin: auto;
     display: flex;
     align-items: center;
+
+    .fieldList {
+        list-style: none;
+        padding-left: 0;
+        width: 80%;
+        display: flex;
+        flex-wrap: wrap;
+        border: 1px solid ${Colors.lightGray};
+        padding: 20px;
+        margin: auto;
+        justify-content: space-between;
+        border-radius: 15px;
+        margin-top: 50px;
+    }
+
+    .checkboxGroupTitle {
+        position: relative;
+        top: 69px;
+        background-color: ${Colors.whiteBlue};
+        font-weight: bold;
+        padding: 10px;
+    }
 `;
 
 
 const GeneralStep = () => {
 
     const dispatch = useDispatch();
-    const { handleSubmit, register, errors } = useForm();
+    const { handleSubmit, register, errors, control } = useForm();
     const formFieldValues = useSelector(state => state.addDeviceFormValues);
     const stepNumber = useSelector(state => state.currentStep);
+    const [deviceTypeFields, setDeviceTypeFields] = useState([]);
+
+    useEffect(() => {
+        setDeviceTypeFields(constructDeviceTypeFields(deviceTypes));
+    }, []);
+
+    const deviceTypes = [
+        'speaker',
+        'dj_mixer',
+        'effects_pedal',
+        'effects_processor',
+        'control_surface',
+        'audio_interface',
+        'sequencer',
+        'turntable',
+        'headphones',
+        'synthesizer',
+        'drum_machine',
+        'mixing_desk',
+        'recorder',
+        'midi_thru_box',
+        'midi_merge_box',
+        'clock_generator',
+        'clock_generators'
+    ]
+
+    const constructDeviceTypeFields = (deviceTypessss) => {
+        const typeFields = [];
+        deviceTypessss.forEach(type => {
+            typeFields.push({
+                "name": "",
+                "id": '_' + Math.random().toString(36).substr(2, 9),
+                "value": type,
+                "label": CapitalizeString(type.replace(/_/g, ' ')),
+                "checked": false
+            })
+        });
+        return typeFields;
+    }
+
+    const CapitalizeString = string => {
+        string = string.split(" ");
+
+        for (var i = 0, x = string.length; i < x; i++) {
+            string[i] = string[i][0].toUpperCase() + string[i].substr(1);
+        }
+
+        return string.join(" ");
+    }
 
     const submitStep = async data => {
         const updatedData = formFieldValues;
@@ -32,6 +104,26 @@ const GeneralStep = () => {
 
     const nextStep = () => {
         dispatch(currentStep(stepNumber + 1));
+    }
+
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: ''
+    });
+
+    const updateChecked = e => {
+        const clickedCheckbox = e.target;
+
+        setDeviceTypeFields(deviceTypeFields.map(field => {
+            if (field.id === clickedCheckbox.id) {
+                if (field.checked === true) {
+                    field.checked = false;
+                } else {
+                    field.checked = true;
+                }
+            }
+            return field;
+        }));
     }
 
     return (
@@ -56,6 +148,22 @@ const GeneralStep = () => {
                                         required: 'Please enter manufacturer'
                                     })} />
                                 </div>
+                                <div className='checkboxGroupTitle'>Device types</div>
+                                <ul className='fieldList'>
+                                    {deviceTypeFields.map((field, index) => (
+                                        <li className='inputListItem' key={index}>
+                                            <CustomCheckBoxStyles>
+                                                <div className='checkboxContainer' >
+                                                    <label className='checkBoxLabel'>
+                                                        {field.label}
+                                                        <input checked={field.checked} onClick={updateChecked} id={field.id} type='checkbox' name='deviceType' value={field.valiue} ref={register()} />
+                                                        <span className='customCheckbox'></span>
+                                                    </label>
+                                                </div>
+                                            </CustomCheckBoxStyles>
+                                        </li>
+                                    ))}
+                                </ul>
                             </div>
                             <StepNavigationButton next iconname='arrow-circle-right' />
                         </div>
--- src/styles/components.js
+++ src/styles/components.js
@@ -2,6 +2,78 @@ import styled from 'styled-components';
 import Colors from '../styles/colors';
 
 
+export const CustomCheckBoxStyles = styled.div`
+    .checkboxContainer {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-start;
+        min-width: 170px;
+        height: 50px;
+
+        .checkBoxLabel {
+            display: block;
+            position: relative;
+            padding-left: 35px;
+            margin-bottom: 12px;
+            cursor: pointer;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        .checkBoxLabel input {
+            position: absolute;
+            opacity: 0;
+            cursor: pointer;
+            height: 0;
+            width: 0;
+        }
+
+        .customCheckbox {
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 25px;
+            width: 25px;
+            border: 1px solid ${Colors.lightGray};
+            background: transparent;
+            border-radius: 5px;
+        }
+
+        .checkBoxLabel:hover input ~ .customCheckbox {
+            background-color: ${Colors.lightGray};
+        }
+
+        .checkBoxLabel input:checked ~ .customCheckbox {
+            background-color: #2196F3;
+        }
+
+        .customCheckbox:after {
+            content: "";
+            position: absolute;
+            display: none;
+        }
+
+        .checkBoxLabel input:checked ~ .customCheckbox:after {
+            display: block;
+        }
+
+        .checkBoxLabel .customCheckbox:after {
+            left: 9px;
+            top: 5px;
+            width: 5px;
+            height: 10px;
+            border: solid white;
+            border-width: 0 3px 3px 0;
+            -webkit-transform: rotate(45deg);
+            -ms-transform: rotate(45deg);
+            transform: rotate(45deg);
+        }
+    }
+`;
+
 export const ConnectionFormSectionStyles = styled.div`
     .connectionFormSection {
         
