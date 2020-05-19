627a8ead762ec234e082656551900fac163d0c01
BernardCooley <bernardcooley@gmail.com>
2020-05-19 16:33:02 +0100

    added middlegray2

--- src/components/SVGWorkspace/SVGWorkspace.js
+++ src/components/SVGWorkspace/SVGWorkspace.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import LayoutDevice from '../SVGWorkspace/LayoutDevice';
 import ConnectionLegend from './ConnectionLegend';
+import Colors from '../../styles/colors';
 
 
 const Styles = styled.div`
     width: 100%;
 
     .svgWorkspaceContainer {
-        background-color: #434343;
+        background-color: ${Colors.middleGray2};
         background-image: linear-gradient(0deg, transparent 24%, rgba(255, 255, 255, .05) 25%, rgba(255, 255, 255, .05) 26%, transparent 27%, transparent 74%, rgba(255, 255, 255, .05) 75%, rgba(255, 255, 255, .05) 76%, transparent 77%, transparent), linear-gradient(90deg, transparent 24%, rgba(255, 255, 255, .05) 25%, rgba(255, 255, 255, .05) 26%, transparent 27%, transparent 74%, rgba(255, 255, 255, .05) 75%, rgba(255, 255, 255, .05) 76%, transparent 77%, transparent);
         background-size: 50px 50px;
         width: auto;
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,6 +1,7 @@
 const Colors = {
     darkTeal: '#224454',
     middleGray: '#5c5e5f',
+    middleGray2: '#434343',
     lightGray: '#c4c9cb',
     whiteBlue: '#f3f7ff',
     whiteBlueOpaque: 'rgba(245, 248, 255, 0.30)',
@@ -9,7 +10,7 @@ const Colors = {
     jet: '#333333',
     black: 'black',
     brightGreen: 'rgba(18, 171, 18, 1)',
-    brightGreenOpaque: 'rgba(18, 171, 18, 0.4)',
+    brightGreenOpaque: 'rgba(18, 171, 18, 0.4)'
 }
 
 export default Colors;
